d9a2f508924d07d2bdfeeb4ad931ac2f0f3885ec
Minwoo Yu <snowypainter@gmail.com>
2024-12-24 20:42:43 +0900

    first commit

---
+++ ex1.py
@@ -0,0 +1,9 @@
+import utils
+
+depth_map = utils.estimate_depth("ex1.Bmp")
+
+#utils.visualize_depth_map(depth_map)
+
+point_cloud = utils.generate_point_cloud(depth_map, utils.create_intrinsic_matrix(depth_map, 1.0))
+
+utils.visualize_point_cloud(point_cloud)
---
+++ utils.py
@@ -0,0 +1,63 @@
+import torch
+import urllib.request
+from torchvision.transforms import Compose, Normalize, Resize, ToTensor
+from PIL import Image
+import cv2
+import numpy as np
+import open3d as o3d
+
+model_type = "DPT_Large"
+midas = torch.hub.load("intel-isl/MiDaS", model_type)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+midas.to(device)
+midas.eval()
+midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
+transform = midas_transforms.default_transform
+
+def estimate_depth(image_path):
+    img = cv2.imread(image_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    input_batch = transform(img).to(device)
+    with torch.no_grad():
+        prediction = midas(input_batch)
+        prediction = torch.nn.functional.interpolate(
+            prediction.unsqueeze(1),
+            size=img.shape[:2],
+            mode="bicubic",
+            align_corners=False,
+        ).squeeze()
+    depth_map = prediction.cpu().numpy()
+    return depth_map
+
+def visualize_depth_map(depth_map):
+    depth_map_normalized = cv2.normalize(depth_map, None, 0, 255, cv2.NORM_MINMAX)
+    depth_map_normalized = np.uint8(depth_map_normalized)
+    depth_map_colored = cv2.applyColorMap(depth_map_normalized, cv2.COLORMAP_MAGMA)
+    cv2.imshow("Depth Map", depth_map_colored)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def create_intrinsic_matrix(depth_map, focal_length):
+    focal_length = 1.0  # 예제 값
+    center = (depth_map.shape[1] / 2, depth_map.shape[0] / 2)
+    intrinsic_matrix = np.array([
+        [focal_length, 0, center[0]],
+        [0, focal_length, center[1]],
+        [0, 0, 1]
+    ])
+    return intrinsic_matrix
+
+def generate_point_cloud(depth_map, intrinsic_matrix):
+    h, w = depth_map.shape
+    i, j = np.meshgrid(np.arange(w), np.arange(h), indexing="xy")
+    z = depth_map.flatten()
+    x = (i.flatten() - intrinsic_matrix[0, 2]) * z / intrinsic_matrix[0, 0]
+    y = (j.flatten() - intrinsic_matrix[1, 2]) * z / intrinsic_matrix[1, 1]
+
+    points = np.vstack((x, y, z)).T
+    return points
+
+def visualize_point_cloud(points):
+    point_cloud_o3d = o3d.geometry.PointCloud()
+    point_cloud_o3d.points = o3d.utility.Vector3dVector(points)
+    o3d.visualization.draw_geometries([point_cloud_o3d])
